91a9fb3e058fa7f25900eaae25fe39c6339bfb3e
JoseDSaramago <josewbmx@gmail.com>
2023-05-04 20:04:45 -0600

    init plugin

---
+++ coupon_discount.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Trendee Coupons Discount
+ * Plugin URI:        https://github.com/JoseDMarck/couponsDiscount
+ * Description:       Aplicar cupones sobre el descuento ahorrado
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            @josedsamamago
+ * Author URI:        https://github.com/JoseDMarck/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+?>
+
